9e29a9f6de84922d993c393e176b62f96010b10d
Monaco Work <lucas.monaco.maquina@gmail.com>
2021-03-04 18:05:32 -0300

    Update first page and women img

--- js/index.js
+++ js/index.js
@@ -1,29 +1,19 @@
 var container = document.querySelector(".container")
 var firstClick = true;
-// var bar = new ldBar(".ldBar");
 
 var startPage = gsap.timeline({ paused: true });
 
+
 startPage
     .addLabel('start')
-    .to('.dia-mulher-text', .7, { scale: 1, opacity: 1, rotationZ: '0.01deg', ease: 'power2.out' }, "start+=" + .1)
-    .to('.logo-text', 1, { opacity: 1, ease: 'power1.out' }, "start+=" + .1)
+    .to('.logo-db-anim', 1, { opacity: 1, scale: 1, rotationZ: '0.01deg', ease: 'back(1.8).out' }, "start+=" + .3)
+    
+    .to('.text-anima', 1, { opacity: 1, y: 0, ease: 'power2.out', stagger: .2 }, "start+=" + .3)
+
     .to('.phone-anim', .8, { opacity: 1, scale: 1, rotationZ: '0.01deg', ease: 'power1.out' }, "start+=" + .4)
     .to('.arrow-anim', .6, { opacity: 1, x: 0, rotationZ: '0.01deg', ease: 'back(1.8).out' }, "start+=" + .6)
-    .to('.loading-text-anim', 1, { opacity: 1, ease: 'power1.out' }, "start+=" + .7)
-    .to('.logo-db-anim', 1, { opacity: 1, scale: 1, rotationZ: '0.01deg', ease: 'back(1.8).out' }, "start+=" + .7)
+    .to('.arrow-anim', 1, { x: 4, repeat: 10, yoyo: true, ease: 'none' }, "start+=" + 1.2)
 
 window.addEventListener("load", function () {
-    // bar.set(100);
-
     startPage.play();
-
-    // document.body.addEventListener("click", function () {
-    //     if (firstClick) {
-    //         gsap.to(container, 1, { opacity: 1, ease: "power2.out" });
-    //         gsap.to(".loading-screen", .3, { opacity: 0, display: 'none', ease: "power2.out" });
-    //         firstClick = false;
-    //     }
-    // })
-    
 })
